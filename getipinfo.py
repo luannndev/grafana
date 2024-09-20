b5272a1af291f337005b80460ac3ff637d733b09
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-20 19:43:51 +0200

    Add initial scripts and configuration for IP logging and analysis
    Created new Bash scripts for extracting and sending IP information. Added Python script for Geo-IP lookups and InfluxDB logging. Included Dockerfile setup and Grafana configuration for displaying connection data.

---
+++ getipinfo.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+import sys
+#import geoip2.webservice
+print ('*************************************')
+print (sys.argv[1])
+
+#print str(sys.argv[1])
+import geoip2.database
+import socket
+
+print(socket.gethostname())
+
+
+
+reader = geoip2.database.Reader('/GeoLite2-City.mmdb')
+response = reader.city(str(sys.argv[1]))
+
+Lat = response.location.latitude
+ISO = response.country.iso_code
+Long = response.location.longitude
+State = response.subdivisions.most_specific.name
+City = response.city.name
+Country = response.country.name
+Zip = response.postal.code
+IP = str(sys.argv[1])
+Domain = str(sys.argv[2])
+time = int(sys.argv[3])
+print (Country)
+print (State)
+print (City)
+print (Zip)
+print (Long)
+print (Lat)
+print (ISO)
+print (IP)
+reader.close()
+
+
+import datetime
+from influxdb import InfluxDBClient
+
+## get env vars and use
+
+import os
+# influx configuration - edit these
+
+npmhome = "/root/.config/NPMGRAF"
+npmhome = os.getenv('NPMGRAF_HOME')
+ifuser = os.getenv('INFLUX_USER')
+ifpass = os.getenv('INFLUX_PW')
+ifdb   = os.getenv('INFLUX_DB')
+ifhost = os.getenv('INFLUX_HOST')
+ifport = os.getenv('INFLUX_PORT')
+
+hostname = socket.gethostname()
+measurement_name = ("ReverseProxyConnections")
+print (measurement_name)
+print ('*************************************')
+
+
+# format the data as a single measurement for influx
+body = [
+    {
+        "measurement": measurement_name,
+        "time": time,
+        "tags": {
+            "key": ISO,
+            "latitude": Lat,
+            "longitude": Long,
+            "Domain": Domain,
+            "City": City,
+            "State": State,
+            "name": Country,
+            "IP": IP
+            },
+        "fields": {
+            "Domain": Domain,
+            "latitude": Lat,
+            "longitude": Long,
+            "State": State,
+            "City": City,
+            "key": ISO,
+            "IP": IP,
+            "name": Country,
+            "metric": 1
+        }
+    }
+]
+
+# connect to influx
+ifclient = InfluxDBClient(ifhost,ifport,ifuser,ifpass,ifdb)
+
+# write the measurement
+ifclient.write_points(body)
---
+++ sendips.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# " /bin/sh /Shared/nginx/shtail.sh & " needs to be added to /startapp.sh as the second line
+# for ipv6 and ipv4 grep -E -o "(([0-9]{1,3}[\.]){3}[0-9]{1,3}|([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))" file.txt
+# for ipv6 and ipv4 exluding homeips grep -E -o "(([0-9]{1,3}[\.]){3}[0-9]{1,3}|([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))" | grep -v "192.168.0.*\| 69.0.0.* \| 5.0.0.*" file.txt
+# for domains grep -E -o "[a-z0-9]*\.[a-z0-9]*\.(de|net|org|com)" file.txt #working for domains
+
+# Check if jq is installed and executable, if not install it and set as executable.  This allows us to parse the json logs
+if [[ -x "/usr/bin/jq" ]]
+then
+    echo "jq available"
+else
+    echo "jq is not executable or found, installing"
+    curl -L -o /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+    chmod +x /usr/bin/jq
+fi
+
+# Tail the custom log location to allow json parsing of the custom logs
+tail -f /logs/*-json.log | while read line;
+
+do
+  #get requested domain from json log and remove the leading and trailing quotes
+  domain=`echo $line | jq '.http_host'  | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/'`
+  #get requesting IP from json log and remove the leading and trailing quotes
+  ipaddressnumber= `echo $line | jq '.remote_addr'  | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/'`
+  #get actual time the request was made from json log and remove the leading and trailing quotes
+  reqtime=`echo $line | jq '.time_iso8601' | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/'`
+
+  #Print on screen captured values
+  echo $reqtime
+  echo $ipaddressnumber
+  echo $domain
+
+  #Get public IP4 and IP6 addresses, so we can avoid sending IPs from our own connection
+  myhomeIP4=$(wget -qO- https://ipv4.icanhazip.com/)
+  myhomeIP6=$(wget -qO- https://ipv6.icanhazip.com/)
+
+   if [[ "$ipaddressnumber" == "$myhomeIP4" ]]
+  then
+    echo "Home IP4"
+  elif [[ "$ipaddressnumber" == "$myhomeIP6" ]]
+  then
+    echo "Home IP6"
+  #LanIP is used to not send your home network ips to keep the number of sends down, edit this to represent your subnet BUT DO NOT REMOVE THE "^" at the start as this represents "starts with" in the regex
+  elif [[ "$ipaddressnumber" =~ ^192.168.0.* ]]
+  then
+    echo "LAN IP"
+  elif [[ -z "$ipaddressnumber" ]]
+  then
+    echo "IP null"
+  else
+    python /root/.config/NPMGRAF/Getipinfo.py "$ipaddressnumber" "$domain" "$reqtime"
+  fi
+done
+reboot
